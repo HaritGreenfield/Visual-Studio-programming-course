807921c3998cfbb520485fa91d3e9cab742cd1b3
HarryGreenfield <harit.thanki@gmail.com>
2022-05-08 14:44:14 +0200

    Generic swap function swaps both char and int without having to create repeat functions.

---
+++ CppTutorial/CppTutorial.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+template<typename T>  
+void Swap(T& a, T& b) {
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+
+int main()
+{
+    int a = 5, b = 7;
+    cout << a << " - " << b << endl;
+    Swap<int>(a, b);                   // <int> decleration is not needed since template function is generic
+    cout << a << " - " << b << endl;
+
+    char c='c', d='d';
+    cout << c << " - " << d << endl;
+    Swap<char>(c, d);                  // <char> decleration is not needed since template function is generic
+    cout << c << " - " << d << endl;
+
+    system("pause>0");
+    return 0;
+}
