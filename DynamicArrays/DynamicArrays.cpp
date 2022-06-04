89f46759407541e5f8c8106f51c837355d735b7e
HarryGreenfield <harit.thanki@gmail.com>
2022-06-04 14:40:13 +0200

    Example on how to allocate a new array and then de-allocating it once it is no longer needed, because the memory space outside the program does not belong to us.

---
+++ DynamicArrays/DynamicArrays.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{   
+    int size;
+    cout << "Size: ";
+    cin >> size;
+    //int myArray[5];
+    int* myArray = new int[size];
+
+    for (int i = 0; i < size; i++) {
+        cout << "Array[" << i << "] ";
+        cin >> myArray[i];
+    }
+
+    for (int i = 0; i < size; i++) {
+        //cout << myArray[i]<<" ";
+        cout << *(myArray+i) << " ";
+    }
+
+    delete[]myArray;
+    myArray = NULL;
+
+    system("pause>0");
+}
+
